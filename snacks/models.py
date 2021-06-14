96769df1142422e35cca4087b8f6839e24813bf7
Rawan199812 <rawan.13579@icloud.com>
2021-06-14 22:51:38 +0300

    done

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+# from django.db.models.fields import CharField
+
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
